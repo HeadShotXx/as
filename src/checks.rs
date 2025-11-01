a9a2715cc5a085d08a310d6ef798ed9fdc2165f4
HeadShotXx <148909166+HeadShotXx@users.noreply.github.com>
2025-11-01 10:06:57 +0300

    Delete src directory

--- src/checks.rs
+++
@@ -1,9 +0,0 @@
-pub fn run_all_checks() -> bool {
-    anti_vm::is_virtualized()
-        || anti_sandbox::check_user_activity()
-        || anti_sandbox::check_for_hooking()
-        || anti_sandbox::check_processes()
-        || anti_sandbox::check_artifacts()
-        || anti_sandbox::check_uptime()
-        || anti_debug_rust::run_all_checks_hidden()
-}
