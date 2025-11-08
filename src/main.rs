dc3a85f05af639da0dd98b1b1db2af4c92753f9f
HeadShotXx <148909166+HeadShotXx@users.noreply.github.com>
2025-11-08 11:00:36 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use my_lib::get_secret_message;
+use obfuscator::obfuscate;
+
+fn main() {
+    let my_secret_string = get_secret_message();
+    println!("Decrypted string from my_lib: {}", my_secret_string);
+	let my_secret_string1 = obfuscate!("Hello, this is a secret message!");
+    println!("Decrypted string: {}", my_secret_string1);
+}
