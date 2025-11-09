6dbe05e3a85147d3553064fab8e49d3d308d9072
HeadShotXx <148909166+HeadShotXx@users.noreply.github.com>
2025-11-09 20:04:21 +0300

    Add files via upload

---
+++ my_lib/src/lib.rs
@@ -0,0 +1,16 @@
+use obfuscator::obfuscate_string;
+
+pub fn get_secret_message() -> String {
+    obfuscate_string!("This is a secret message from my_lib!").to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let message = get_secret_message();
+        assert_eq!(message, "This is a secret message from my_lib!");
+    }
+}
