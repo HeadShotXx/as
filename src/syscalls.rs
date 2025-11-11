0718cf8ea4810614cef8eeff09817cc03b2400eb
HeadShotXx <148909166+HeadShotXx@users.noreply.github.com>
2025-11-11 10:55:37 +0300

    Add files via upload

---
+++ src/syscalls.rs
@@ -0,0 +1,111 @@
+use std::mem::transmute;
+use once_cell::sync::Lazy;
+use std::ffi::c_void;
+use windows_sys::Win32::System::Threading::PROCESS_INFORMATION_CLASS;
+use windows_sys::Win32::System::LibraryLoader::{LoadLibraryA, GetProcAddress};
+type NtQueryInformationProcess = extern "system" fn(
+ProcessHandle: *mut c_void,
+ProcessInformationClass: PROCESS_INFORMATION_CLASS,
+ProcessInformation: *mut c_void,
+ProcessInformationLength: u32,
+ReturnLength: *mut u32,
+) -> i32;
+type NtClose = extern "system" fn(Handle: *mut c_void) -> i32;
+type NtReadVirtualMemory = extern "system" fn(
+ProcessHandle: *mut c_void,
+BaseAddress: *mut c_void,
+Buffer: *mut c_void,
+NumberOfBytesToRead: usize,
+NumberOfBytesRead: *mut usize,
+) -> i32;
+type NtWriteVirtualMemory = extern "system" fn(
+ProcessHandle: *mut c_void,
+BaseAddress: *mut c_void,
+Buffer: *mut c_void,
+NumberOfBytesToWrite: usize,
+NumberOfBytesWritten: *mut usize,
+) -> i32;
+type NtResumeThread = extern "system" fn(
+    ThreadHandle: *mut c_void,
+    SuspendCount: *mut u32,
+) -> i32;
+
+type NtAllocateVirtualMemory = extern "system" fn(
+    ProcessHandle: *mut c_void,
+    BaseAddress: *mut *mut c_void,
+    ZeroBits: usize,
+    RegionSize: *mut usize,
+    AllocationType: u32,
+    Protect: u32,
+) -> i32;
+
+type NtProtectVirtualMemory = extern "system" fn(
+    ProcessHandle: *mut c_void,
+    BaseAddress: *mut *mut c_void,
+    RegionSize: *mut usize,
+    NewProtect: u32,
+    OldProtect: *mut u32,
+) -> i32;
+
+type NtFlushInstructionCache = extern "system" fn(
+    ProcessHandle: *mut c_void,
+    BaseAddress: *mut c_void,
+    RegionSize: usize,
+) -> i32;
+
+#[derive(Clone)]
+pub struct Syscalls {
+    pub NtQueryInformationProcess: NtQueryInformationProcess,
+    pub NtClose: NtClose,
+    pub NtReadVirtualMemory: NtReadVirtualMemory,
+    pub NtWriteVirtualMemory: NtWriteVirtualMemory,
+    pub NtResumeThread: NtResumeThread,
+    pub NtAllocateVirtualMemory: NtAllocateVirtualMemory,
+    pub NtProtectVirtualMemory: NtProtectVirtualMemory,
+    pub NtFlushInstructionCache: NtFlushInstructionCache,
+}
+
+impl Syscalls {
+    fn new() -> Result<Syscalls, &'static str> {
+        unsafe {
+            let ntdll = LoadLibraryA("ntdll.dll\0".as_ptr());
+            if (ntdll as *mut std::ffi::c_void).is_null() {
+                return Err("Failed to load ntdll.dll");
+            }
+
+            let NtQueryInformationProcess = GetProcAddress(ntdll, "NtQueryInformationProcess\0".as_ptr());
+            let NtClose = GetProcAddress(ntdll, "NtClose\0".as_ptr());
+            let NtReadVirtualMemory = GetProcAddress(ntdll, "NtReadVirtualMemory\0".as_ptr());
+            let NtWriteVirtualMemory = GetProcAddress(ntdll, "NtWriteVirtualMemory\0".as_ptr());
+            let NtResumeThread = GetProcAddress(ntdll, "NtResumeThread\0".as_ptr());
+            let NtAllocateVirtualMemory = GetProcAddress(ntdll, "NtAllocateVirtualMemory\0".as_ptr());
+            let NtProtectVirtualMemory = GetProcAddress(ntdll, "NtProtectVirtualMemory\0".as_ptr());
+            let NtFlushInstructionCache = GetProcAddress(ntdll, "NtFlushInstructionCache\0".as_ptr());
+            if NtQueryInformationProcess.is_none()
+                || NtClose.is_none()
+                || NtReadVirtualMemory.is_none()
+                || NtWriteVirtualMemory.is_none()
+                || NtResumeThread.is_none()
+                || NtAllocateVirtualMemory.is_none()
+                || NtProtectVirtualMemory.is_none()
+                || NtFlushInstructionCache.is_none()
+            {
+                return Err("Failed to get one or more function addresses");
+            }
+
+            Ok(Syscalls {
+                NtQueryInformationProcess: transmute(NtQueryInformationProcess.unwrap()),
+                NtClose: transmute(NtClose.unwrap()),
+                NtReadVirtualMemory: transmute(NtReadVirtualMemory.unwrap()),
+                NtWriteVirtualMemory: transmute(NtWriteVirtualMemory.unwrap()),
+                NtResumeThread: transmute(NtResumeThread.unwrap()),
+                NtAllocateVirtualMemory: transmute(NtAllocateVirtualMemory.unwrap()),
+                NtProtectVirtualMemory: transmute(NtProtectVirtualMemory.unwrap()),
+                NtFlushInstructionCache: transmute(NtFlushInstructionCache.unwrap()),
+            })
+        }
+    }
+}
+pub static SYSCALLS: Lazy<Syscalls> = Lazy::new(|| {
+Syscalls::new().expect("")
+});
