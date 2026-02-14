ff170cbaa2ff426ee6e01dfd97f6111d61d18ea7
HeadShotXx <148909166+HeadShotXx@users.noreply.github.com>
2026-02-14 11:01:22 +0300

    Delete src directory

--- src/test_simple.rs
+++
@@ -1,9 +0,0 @@
-use polimorphic::str_obf;
-
-fn main() {
-    let s1 = str_obf!("Test string 1");
-    let s2 = str_obf!("Test string 2");
-    assert_eq!(s1, "Test string 1", "String 1 mismatch: {}", s1);
-    assert_eq!(s2, "Test string 2", "String 2 mismatch: {}", s2);
-    println!("OK");
-}
