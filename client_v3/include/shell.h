407c7a9d01a2527ead6d5e674af4c9df83a769dd
PayloadExecuter <148909166+HeadShotXx@users.noreply.github.com>
2026-04-17 09:38:09 +0300

    Delete client_v3 directory

--- client_v3/include/shell.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-#include <windows.h>
-
-char* run_powershell(const char* cmd);
-char* run_cmd(const char* cmd);
-
-#endif
--- client_v3/include/sysinfo.h
+++
@@ -1,6 +0,0 @@
-#ifndef SYSINFO_H
-#define SYSINFO_H
-
-char* collect_sysinfo();
-
-#endif
