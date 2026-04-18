8e8073cfe2802f17fd153ed2ac15dd6f9068ac5a
PayloadExecuter <148909166+HeadShotXx@users.noreply.github.com>
2026-04-18 10:35:41 +0300

    Delete client directory

--- client/include/browser.h
+++
@@ -1,9 +0,0 @@
-#ifndef BROWSER_H
-#define BROWSER_H
-
-#include <windows.h>
-#include "utils.h"
-
-void collect_browser_data(const char* browser_name, SOCKET sock, HANDLE mutex);
-
-#endif
--- client/include/camera.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAMERA_H
-#define CAMERA_H
-
-#include <windows.h>
-#include "utils.h"
-
-void camera_stream_loop(SOCKET sock, HANDLE mutex, HANDLE stop_event, int fps);
-
-#endif
