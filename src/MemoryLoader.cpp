14800fdf01909f47576fa07e497de0567442192c
PayloadExecuter <148909166+HeadShotXx@users.noreply.github.com>
2026-04-30 19:56:25 +0300

    Delete src directory

--- src/MemoryLoader.cpp
+++
@@ -1,7 +0,0 @@
-#include "MemoryLoader.hpp"
-
-HMODULE MemoryLoader::Load(const std::vector<unsigned char>& buffer) {
-    // Gelecekte buraya Reflective DLL veya Manual Mapping kodları gelecek.
-    // Şimdilik hata vermemesi için NULL dönüyoruz.
-    return NULL; 
-}
