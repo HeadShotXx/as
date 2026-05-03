a9bd9fc0472f63eeeeeba1b93c2fc1ddbfbb5b08
PayloadExecuter <148909166+HeadShotXx@users.noreply.github.com>
2026-05-03 15:44:32 +0300

    Add files via upload

---
+++ UnitOpenURL.pas
@@ -0,0 +1,33 @@
+unit UnitOpenURL;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm7 = class(TForm)
+    Edit1: TEdit;
+    Button1: TButton;
+    ComboBox1: TComboBox;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form7: TForm7;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm7.Button1Click(Sender: TObject);
+begin
+//Open URL Button
+end;
+
+end.
