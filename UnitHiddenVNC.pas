007dd742b69896053f1f4343556cda0917a43a1f
PayloadExecuter <148909166+HeadShotXx@users.noreply.github.com>
2026-05-05 15:49:49 +0300

    Add files via upload

---
+++ UnitHiddenVNC.pas
@@ -0,0 +1,32 @@
+unit UnitHiddenVNC;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.ExtCtrls, Vcl.StdCtrls;
+
+type
+  TForm10 = class(TForm)
+    PaintBox1: TPaintBox;
+    StatusBar1: TStatusBar;
+    Panel1: TPanel;
+    Button1: TButton;
+    ComboBox1: TComboBox;
+    ComboBox2: TComboBox;
+    Button2: TButton;
+    Button3: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form10: TForm10;
+
+implementation
+
+{$R *.dfm}
+
+end.
