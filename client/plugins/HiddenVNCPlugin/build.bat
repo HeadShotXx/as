82d68284e262fae43c25384759c1463de6bb5464
PayloadExecuter <148909166+HeadShotXx@users.noreply.github.com>
2026-05-09 10:36:49 +0300

    Delete client directory

--- client/plugins/HiddenVNCPlugin/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not exist "build" mkdir build
-g++ -O2 -shared main.cpp -o build/HiddenVNCPlugin.dll -lws2_32 -lgdiplus -lgdi32 -luser32 -lole32 -static-libgcc -static-libstdc++ -static
-if %errorlevel% neq 0 (
-    echo Build failed!
-    exit /b %errorlevel%
-)
-echo Build successful: build/HiddenVNCPlugin.dll
